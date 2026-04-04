b891abe7301a45368b82d358acd8c2529386b218
anna-shalygina <annash26112006@gmail.com>
2026-04-04 19:07:33 +0400

    Iterative version with all numbers

--- program.py
+++ program.py
@@ -1,8 +1,13 @@
 n=int(input())
 def fib(n):
     if n==1:
-        return 0
+        return [0]
     if n==2:
-        return 1
-    return fib(n-1)+fib(n-2)
-print(fib(n))
+        return [0,1]
+    res=[0,1]
+    for i in range(2,n):
+        res.append(res[i-1]+res[i-2])
+    return res
+num=fib(n)
+for i in num:
+    print(i)
